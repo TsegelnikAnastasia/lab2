ab4620ab45b662b80cfa18bddf4f6e788e998660
TsegelnikAnastasia <stasamar29@gmail.com>
2026-02-25 00:15:57 +0300

    Update CompNandD.cpp

--- CompNandD.cpp
+++ CompNandD.cpp
@@ -3,32 +3,89 @@ d1, d2, …, d8. Верно ли, что произведение вещественных
 чисел больше n? */
 
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
+double calculateProd(double numbers[], int size) {
+	double Prod = 1.0;
+	for (int i = 0; i < size; i++) {
+		Prod *= numbers[i];
+	}
+	return Prod;
+}
+
+void Test1() {
+	double numbers[] = { 2,2,2,2,2,2,2,2 };
+	assert(calculateProd(numbers, 8) == 256);
+	cout << "Тест 1 пройден успешно" << endl;
+}
+
+void Test2() {
+	double numbers[] = { 0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5 };
+	assert(calculateProd(numbers, 8) == 0.00390625);
+	cout << "тест 2 пройден успешно" << endl;
+
+}
+
+void Test3() {
+	double numbers[] = { 0,5,5,5,1,1,1,1 };
+	assert(calculateProd(numbers, 8) == 0);
+	cout << "Тест 3 пройден успешно" << endl;
+}
+
+void Test4() {
+	double numbers[] = { 2,4,2,2,4,2,2,2 };
+	assert(calculateProd(numbers, 8) > 1000);
+	assert((calculateProd(numbers, 8) > 3000) == false);
+	cout << "Тест 4 пройден успешно" << endl;
+}
+
+void Test5() {
+	double numbers[] = { 1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0 };
+	assert(calculateProd(numbers, 8) == 1);
+	assert((calculateProd(numbers, 8) > 1) == false);
+	cout << "Тест 5 пройден успешно" << endl;
+}
+
+void RunAllTest() {
+	Test1();
+	Test2();
+	Test3();
+	Test4();
+	Test5();
+	cout << "все тесты выполнены успешно" << endl;
+}
+
 int main() {
 	setlocale(LC_ALL, "russian");
-
-	int n;
-	double d1, d2, d3, d4, d5, d6, d7, d8;
-	double composition;
-	cout << "Введите натуральоне число n " << endl;
-	cin >> n;
-	if (n < 1) {
-		cout << "Не натуралное число";
-		return 1;
+	int choice;
+	cout << "1 - Программа, 2 - Тесты:";
+	cin >> choice;
+	if (choice == 2) {
+		RunAllTest();
+		return 0;
 	}
-	cout << "Введите вещетсвенные числа d1, d2, …, d8" << endl;
-	cin >> d1 >> d2 >> d3 >> d4 >> d5 >> d6 >> d7 >> d8;
-	composition = d1 * d2 * d3 * d4 * d5 * d6 * d7 * d8;
-	if (composition > n) {
-		cout << "Произведение больше натурального числа - верно" << endl;
-	}
-	else if (composition < n) {
-		cout << "произвдение меньше натурльного числа - неверно" << endl;
-	}
-	else cout << "равны - неверно " << endl;
+	else if (choice == 1) {
+		int n;
+		const int size = 8;
+		double numbers[size];
 
+		cout << "Введите n" << endl;
+		cin >> n;
 
+		if (n < 1) {
+			cout << "Ошибка: число должно быть натуральным (>= 1)" << endl;
+			return 1;
+		}
+		cout << "Введите " << size << " вещественнных чисел" << endl;
+		for (int i = 0; i < size; i++)
+			cin >> numbers[i];
+		double prod = calculateProd(numbers, size);
+		cout << "Произведение = " << prod << endl;
+		if (prod > n) cout << "Верно: произведение больше " << n << endl;
+		else cout << "Неверно: произведение не больше " << n << endl;
+	}
+	else cout << "неверный выбор" << endl;
 	return 0;
 }
