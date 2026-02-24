ca2d886b39e77307067018bbf8620549c2616651
Anastasia <stasamar29@gmail.com>
2026-02-24 16:52:46 +0300

    Update DivisorsSum.cpp

--- DivisorsSum.cpp
+++ DivisorsSum.cpp
@@ -6,9 +6,10 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "russian");
 	int sum = 0;
+	bool IsAre = false;
 	cout << "все целые числа из промежутка от 100 до 300, у которых сумма делителей равна 50" << endl;
 	for (int i = 100; i <= 300; i++) {
-			sum = 0;
+		sum = 0;
 		for (int j = 1; j < i; j++) {
 			if (i % j == 0) {
 				sum += j;
@@ -16,8 +17,9 @@ int main() {
 		}
 		if (sum == 50) {
 			cout << i << " ";
+			IsAre = true;
 		}
 	}
-
+	if (!IsAre) cout << "таких числе нет";
 	return 0;
-}
+
